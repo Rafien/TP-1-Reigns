52b7f966b781a089e4a853a0987d7ba825c21b67
Raf <raphael.choquet@gmail.com>
2023-03-12 19:07:02 +0100

    Moved jauge creation to function createJauge

--- src/main/java/main/Personnage.java
+++ src/main/java/main/Personnage.java
@@ -46,13 +46,16 @@ public class Personnage {
         this.genre = genre;
 
         // Initialisation des jauges entre 15 et 35 points
-        //mettre ailleurs
+        createJauges();
+
+    }
+
+    private void createJauges(){
         jaugeClerge = new Jauge("Clergé", 15 + (int)(Math.random() * (35 - 15)));
         jaugePeuple = new Jauge("Peuple", 15 + (int)(Math.random() * (35 - 15)));
         jaugeArmee = new Jauge("Armée", 15 + (int)(Math.random() * (35 - 15)));
         jaugeFinance = new Jauge("Finances", 15 + (int)(Math.random() * (35 - 15)));
     }
-
     /**
      * Cette fonction permet d'initialiser le personnage joué. Elle demande à l'utilisateur de saisir le nom du personnage
      * et le genre (Roi ou Reine). Elle crée ensuite le personnage.
